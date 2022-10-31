f5f7d09e0ff27d9299ec2299d7416b5eebce904f
田中宙 <tanakachuu@tanakachuunoAir.elecom>
2022-10-31 16:08:09 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
+
+  private
+
+  def production?   #←本番環境かローカルを区別する。
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
+
 end
