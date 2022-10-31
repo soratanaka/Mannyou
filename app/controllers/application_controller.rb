30107e4d91acee98615ce70ce554f30cbaab14ca
田中宙 <tanakachuu@tanakachuunoAir.elecom>
2022-10-31 16:18:57 +0900

    basic fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :production?
+  before_action :basic if Rails.env == "production"
 
   private
-
-  def production?   #←本番環境かローカルを区別する。
-    Rails.env.production?
-  end
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+  def basic
+    authenticate_or_request_with_http_basic do |name, password|
+      name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
-
 end
