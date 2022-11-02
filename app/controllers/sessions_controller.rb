e1961f958ade6e0677bbf86a0d6632c010eb6829
田中宙 <tanakachuu@tanakachuunoAir.elecom>
2022-11-02 10:43:55 +0900

    User make

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,36 @@
+class SessionsController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+
+  def new
+  end
+
+  def create
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+    if login(email, password)
+      flash[:success] = 'ログインに成功しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ログインに失敗しました。'
+      render 'new'
+    end
+  end
+  
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
+  end
+
+  private
+
+  def login(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      session[:user_id] = @user.id
+      return true
+    else
+      return false
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'tasks#index'
+  root 'sessions#new'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
   resources :tasks do
     get :search, on: :collection
   end
